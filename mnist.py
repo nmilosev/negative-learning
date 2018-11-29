bd5f5dbc1f723b826d320e958e2f922a0fe27cac
nmilosev <nmilosevnm@gmail.com>
2018-11-29 12:47:48 +0100

    spremno za NNW

--- mnist.py
+++ mnist.py
@@ -4,35 +4,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from tqdm import tqdm
 import time
-import sys
 
+torch.manual_seed(1)
 
-torch.manual_seed(42)
-
+LR = 0.1
+MOM = 0.5
+HIDDEN = 50 
 
 class Net(nn.Module):
     def __init__(self, net_type):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=5)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=5)
-        self.conv3 = nn.Conv2d(16, 32, kernel_size=5)
-        self.conv3_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(288, 512)
-        self.fc2 = nn.Linear(512, 10)
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, HIDDEN)
+        self.fc2 = nn.Linear(HIDDEN, 10)
         self.net_type = net_type
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x))
-        x = F.leaky_relu(F.max_pool2d(self.conv2(x), 2))
-        x = F.leaky_relu(F.max_pool2d(self.conv3_drop(self.conv3(x)), 2))
-        x = x.view(-1, 288)
-        #print(x.size())
-        if self.net_type == 'negative':  # -x
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        if self.net_type == 'negative':  
             x = x.neg()
-        if self.net_type == 'negative_relu' or self.net_type == 'hybrid':  # 1 - x
+        if self.net_type == 'negative_relu' or 'hybrid' in self.net_type: 
             x = torch.ones_like(x).add(x.neg())
-        x = F.leaky_relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
@@ -61,7 +60,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()  # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
@@ -73,7 +72,7 @@ def test(model, device, test_loader):
 
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
-kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+kwargs = {'num_workers': 12, 'pin_memory': True} if use_cuda else {}
 
 
 def mnist_loader(train=False):
@@ -87,53 +86,51 @@ def mnist_loader(train=False):
 
 train_loader = mnist_loader(train=True)
 test_loader = mnist_loader()
-test_loader_vertical_cut1 = mnist_loader()
-test_loader_vertical_cut2 = mnist_loader()
-test_loader_horizontal_cut1 = mnist_loader()
-test_loader_horizontal_cut2 = mnist_loader()
-test_loader_diagonal_cut1 = mnist_loader()
-test_loader_diagonal_cut2 = mnist_loader()
+test_loader_vertical_cut = mnist_loader()
+test_loader_horizontal_cut = mnist_loader()
+test_loader_diagonal_cut = mnist_loader()
+test_loader_triple_cut = mnist_loader()
 
-print('Generating additional datasets...')
+print('Generating new test sets...')
 
-if '--skipgen' not in sys.argv:
-    for num in range(0, 10000):
-        for x in range(28):
-            for y in range(28):
-                if y < 14:
-                    test_loader_vertical_cut1.dataset.test_data[num, x, y] = 0
-                if y >= 14:
-                    test_loader_vertical_cut2.dataset.test_data[num, x, y] = 0
-                if x < 14:
-                    test_loader_horizontal_cut1.dataset.test_data[num, x, y] = 0
-                if x >= 14:
-                    test_loader_horizontal_cut2.dataset.test_data[num, x, y] = 0
-                if (x < 14 and y > 14) or (x > 14 and y < 14):
-                    test_loader_diagonal_cut1.dataset.test_data[num, x, y] = 0
-                if (x >= 14 and y < 14) or (x < 14 and y >= 14):
-                    test_loader_diagonal_cut2.dataset.test_data[num, x, y] = 0
-
+for num in tqdm(range(0, 10000)):
+    for x in range(28):
+        for y in range(28):
+            if y < 14:
+                test_loader_vertical_cut.dataset.test_data[num, x, y] = 0
+            if x < 14:
+                test_loader_horizontal_cut.dataset.test_data[num, x, y] = 0
+            if (x < 14 and y > 14) or (x > 14 and y < 14):
+                test_loader_diagonal_cut.dataset.test_data[num, x, y] = 0
+            if (5 < x < 15 and 5 <  y < 15) or (17 < x < 27 and 10 < y < 20) or (7 <  x < 17 and 16 < y < 26):  
+                test_loader_triple_cut.dataset.test_data[num, x, y] = 0
 
 model_normal = Net('normal').to(device)
-model_negative = Net('negative').to(device)
+# model_negative = Net('negative').to(device)
 model_negative_relu = Net('negative_relu').to(device)
 model_hybrid = Net('normal').to(device)
+model_hybrid_nr = Net('normal').to(device)
+model_hybrid_alt = Net('normal').to(device)
 
-optimizer_normal = optim.SGD(filter(lambda p: p.requires_grad, model_normal.parameters()), lr=0.05, momentum=0.5)
-optimizer_negative = optim.SGD(filter(lambda p: p.requires_grad, model_negative.parameters()), lr=0.05, momentum=0.5)
-optimizer_negative_relu = optim.SGD(filter(lambda p: p.requires_grad, model_negative_relu.parameters()), lr=0.05, momentum=0.5)
-optimizer_hybrid = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid.parameters()), lr=0.05, momentum=0.5)
+optimizer_normal = optim.SGD(filter(lambda p: p.requires_grad, model_normal.parameters()), lr=LR, momentum=MOM)
+# optimizer_negative = optim.SGD(filter(lambda p: p.requires_grad, model_negative.parameters()), lr=LR, momentum=MOM)
+optimizer_negative_relu = optim.SGD(filter(lambda p: p.requires_grad, model_negative_relu.parameters()), lr=LR, momentum=MOM)
+optimizer_hybrid = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid.parameters()), lr=LR, momentum=MOM)
+optimizer_hybrid_nr = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid_nr.parameters()), lr=LR, momentum=MOM)
+optimizer_hybrid_alt = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid_alt.parameters()), lr=LR, momentum=MOM)
 
 start_time = time.time()
 
 for epoch in range(1, 10 + 1):
     train(model_normal, device, train_loader, optimizer_normal, epoch)
 
-for epoch in range(1, 10 + 1):
-    train(model_negative, device, train_loader, optimizer_negative, epoch)
+# for epoch in range(1, 10 + 1):
+#     train(model_negative, device, train_loader, optimizer_negative, epoch)
 
 for epoch in range(1, 10 + 1):
     train(model_negative_relu, device, train_loader, optimizer_negative_relu, epoch)
+
+# ---- Hybrid net:
 
 for epoch in range(1, 10 + 1):
     train(model_hybrid, device, train_loader, optimizer_hybrid, epoch)
@@ -141,36 +138,77 @@ for epoch in range(1, 10 + 1):
 # change network type
 model_hybrid.net_type = 'hybrid'
 # reinitialize fully connected layers
-model_hybrid.fc1 = nn.Linear(288, 512).cuda()
-model_hybrid.fc2 = nn.Linear(512, 10).cuda()
+model_hybrid.fc1 = nn.Linear(320, HIDDEN).cuda()
+model_hybrid.fc2 = nn.Linear(HIDDEN, 10).cuda()
 # freeze convolutional layers
 model_hybrid.conv1.weight.requires_grad = False
 model_hybrid.conv2.weight.requires_grad = False
-model_hybrid.conv3.weight.requires_grad = False
 # reinitialize the optimizer with new params
-optimizer_hybrid = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid.parameters()), lr=0.05, momentum=0.5)
+optimizer_hybrid = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid.parameters()), lr=LR, momentum=MOM)
 
-for epoch in range(11, 15 + 1):
+for epoch in range(11, 20 + 1):
     train(model_hybrid, device, train_loader, optimizer_hybrid, epoch)
 
-loaders = [test_loader, test_loader_horizontal_cut1, test_loader_horizontal_cut2,
-            test_loader_vertical_cut1, test_loader_vertical_cut2, test_loader_diagonal_cut1,
-            test_loader_diagonal_cut2]
-loader_names = ['Normal data set', 'Horizontal cut LEFT', 'Horizontal cut RIGHT', 'Vertical cut TOP',
-                'Vertical cut BOTTOM', 'Diagonal cut RTL', 'Diagonal cut LTR']
+# ---- Hybrid no reset:
 
-for loader, name in zip(loaders, loader_names):
-    print(name)
-    test(model_normal, device, loader)
-    test(model_negative, device, loader)
-    test(model_negative_relu, device, loader)
-    test(model_hybrid, device, loader)
+for epoch in range(1, 10 + 1):
+    train(model_hybrid_nr, device, train_loader, optimizer_hybrid_nr, epoch)
+
+# change network type
+model_hybrid_nr.net_type = 'hybrid_nr'
+# DO NOT reinitialize fully connected layers
+# freeze convolutional layers
+model_hybrid_nr.conv1.weight.requires_grad = False
+model_hybrid_nr.conv2.weight.requires_grad = False
+# reinitialize the optimizer with new params
+optimizer_hybrid_nr = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid_nr.parameters()), lr=LR, momentum=MOM)
+
+for epoch in range(11, 20 + 1):
+    train(model_hybrid_nr, device, train_loader, optimizer_hybrid_nr, epoch)
+
+# ---- Hybrid alternating:
+
+for epoch in range(1, 10 + 1):
+    train(model_hybrid_alt, device, train_loader, optimizer_hybrid_alt, epoch)
+
+# change network type
+model_hybrid_alt.net_type = 'hybrid_alt'
+# reinitialize fully connected layers
+model_hybrid_alt.fc1 = nn.Linear(320, HIDDEN).cuda()
+model_hybrid_alt.fc2 = nn.Linear(HIDDEN, 10).cuda()
+# freeze convolutional layers
+model_hybrid_alt.conv1.weight.requires_grad = False
+model_hybrid_alt.conv2.weight.requires_grad = False
+# reinitialize the optimizer with new params
+optimizer_hybrid_alt = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid_alt.parameters()), lr=LR, momentum=MOM)
+
+for epoch in range(11, 20 + 1):
+    if epoch % 2:
+        model_hybrid_alt.net_type = 'normal'
+    else:
+        model_hybrid_alt.net_type = 'hybrid_alt'
+    
+    train(model_hybrid_alt, device, train_loader, optimizer_hybrid_alt, epoch)
+
+# Testing:
+
+models = [model_normal, model_negative_relu, model_hybrid, model_hybrid_nr, model_hybrid_alt]
+model_names = ['Normal:', 'HCUT:', 'VCUT:', 'DCUT:', 'TCUT:']
+
+datasets = [test_loader, test_loader_horizontal_cut, test_loader_vertical_cut, test_loader_diagonal_cut, test_loader_triple_cut]
+
+for i, dataset in enumerate(datasets):
+    print('Testing -- ' + model_names[i])
+    for model in models:
+        test(model, device, dataset)
 
 print('--- Total time: %s seconds ---' % (time.time() - start_time))
 
 torch.save(model_normal, 'models/model_normal.pytorch')
-torch.save(model_negative, 'models/model_negative.pytorch')
+# torch.save(model_negative, 'models/model_negative.pytorch')
 torch.save(model_negative_relu, 'models/model_negative_relu.pytorch')
 torch.save(model_hybrid, 'models/model_hybrid.pytorch')
+torch.save(model_hybrid_nr, 'models/model_hybrid_nr.pytorch')
+torch.save(model_hybrid_alt, 'models/model_hybrid_alt.pytorch')
 
 print('models saved to "models"')
