4d1b7bea0d9a4347cb18f2f790c9f418840956db
Nemanja Milosevic <nmilosevnm@gmail.com>
2019-08-21 12:48:56 +0200

    emnist

--- mnist.py
+++ mnist.py
@@ -8,34 +8,37 @@ from tqdm import tqdm
 import time
 
 torch.manual_seed(1)
+#torch.backends.cudnn.deterministic = True
+#torch.backends.cudnn.benchmark = False
+#import numpy as np
+#np.random.seed(0)
 
-LR = 0.1
+LR = 0.01
 MOM = 0.5
-HIDDEN = 50
+HIDDEN = 500
+NUM_CLASSES = 10
+CONV_OUT = 4 * 4 * 50
 
 class Net(nn.Module):
     def __init__(self, net_type):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, HIDDEN)
-        self.fc2 = nn.Linear(HIDDEN, 10)
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(CONV_OUT, HIDDEN)
+        self.fc2 = nn.Linear(HIDDEN, NUM_CLASSES)
         self.net_type = net_type
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        if self.net_type == 'negative':
-            x = x.neg()
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, CONV_OUT)
         if self.net_type == 'negative_relu' or 'hybrid' in self.net_type:
             x = torch.ones_like(x).add(x.neg())
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
-
 
 def train(model, device, train_loader, optimizer, epoch):
     model.train()
@@ -72,67 +75,56 @@ def test(model, device, test_loader):
 
 use_cuda = torch.cuda.is_available()
 device = torch.device('cuda' if use_cuda else 'cpu')
-kwargs = {'num_workers': 12, 'pin_memory': True} if use_cuda else {}
-
+kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
 def mnist_loader(train=False):
     return torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=train, transform=transforms.Compose([
+        datasets.MNIST('../data', download=True, train=train, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])),
         batch_size=64 if train else 1000, shuffle=True, **kwargs)
 
+def emnist_loader(split='mnist'):
+    def _loader(train=False):
+        return torch.utils.data.DataLoader(
+           datasets.EMNIST('../data', split=split, download=True, train=train, transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,))
+            ])),
+            batch_size=64 if train else 1000, shuffle=True, **kwargs)
+    return _loader
 
-train_loader = mnist_loader(train=True)
-test_loader = mnist_loader()
-test_loader_vertical_cut = mnist_loader()
-test_loader_horizontal_cut = mnist_loader()
-test_loader_diagonal_cut = mnist_loader()
-test_loader_triple_cut = mnist_loader()
-
-
-
+loader = mnist_loader
+    
+train_loader = loader(train=True)
+test_loader = loader()
+test_loader_vertical_cut = loader()
+test_loader_horizontal_cut = loader()
+test_loader_diagonal_cut = loader()
+test_loader_triple_cut = loader()
 
 print('Generating new test sets...')
 
-for num in tqdm(range(0, 10000)):
+for num in tqdm(range(0, len(test_loader.dataset))):
     for x in range(28):
         for y in range(28):
             if y < 14:
-                test_loader_vertical_cut.dataset.test_data[num, x, y] = 0
+                test_loader_vertical_cut.dataset.data[num, x, y] = 0
             if x < 14:
-                test_loader_horizontal_cut.dataset.test_data[num, x, y] = 0
+                test_loader_horizontal_cut.dataset.data[num, x, y] = 0
             if (x < 14 and y > 14) or (x > 14 and y < 14):
-                test_loader_diagonal_cut.dataset.test_data[num, x, y] = 0
+                test_loader_diagonal_cut.dataset.data[num, x, y] = 0
             if (5 < x < 15 and 5 <  y < 15) or (17 < x < 27 and 10 < y < 20) or (7 <  x < 17 and 16 < y < 26):
-                test_loader_triple_cut.dataset.test_data[num, x, y] = 0
-
-# import matplotlib.pyplot as plt
-
-# plt.imshow(test_loader.dataset.test_data[343], cmap='gray')
-# plt.show()
-# plt.imshow(test_loader_vertical_cut.dataset.test_data[343], cmap='gray')
-# plt.show()
-# plt.imshow(test_loader_horizontal_cut.dataset.test_data[343], cmap='gray')
-# plt.show()
-# plt.imshow(test_loader_diagonal_cut.dataset.test_data[343], cmap='gray')
-# plt.show()
-# plt.imshow(test_loader_triple_cut.dataset.test_data[343], cmap='gray')
-# plt.show()
-
-# import sys
-# sys.exit(0)
+                test_loader_triple_cut.dataset.data[num, x, y] = 0
 
 model_normal = Net('normal').to(device)
-# model_negative = Net('negative').to(device)
 model_negative_relu = Net('negative_relu').to(device)
 model_hybrid = Net('normal').to(device)
 model_hybrid_nr = Net('normal').to(device)
 model_hybrid_alt = Net('normal').to(device)
 
 optimizer_normal = optim.SGD(filter(lambda p: p.requires_grad, model_normal.parameters()), lr=LR, momentum=MOM)
-# optimizer_negative = optim.SGD(filter(lambda p: p.requires_grad, model_negative.parameters()), lr=LR, momentum=MOM)
 optimizer_negative_relu = optim.SGD(filter(lambda p: p.requires_grad, model_negative_relu.parameters()), lr=LR, momentum=MOM)
 optimizer_hybrid = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid.parameters()), lr=LR, momentum=MOM)
 optimizer_hybrid_nr = optim.SGD(filter(lambda p: p.requires_grad, model_hybrid_nr.parameters()), lr=LR, momentum=MOM)
@@ -142,9 +134,6 @@ start_time = time.time()
 
 for epoch in range(1, 10 + 1):
     train(model_normal, device, train_loader, optimizer_normal, epoch)
-
-# for epoch in range(1, 10 + 1):
-#     train(model_negative, device, train_loader, optimizer_negative, epoch)
 
 for epoch in range(1, 10 + 1):
     train(model_negative_relu, device, train_loader, optimizer_negative_relu, epoch)
@@ -157,8 +146,8 @@ for epoch in range(1, 10 + 1):
 # change network type
 model_hybrid.net_type = 'hybrid'
 # reinitialize fully connected layers
-model_hybrid.fc1 = nn.Linear(320, HIDDEN).cuda()
-model_hybrid.fc2 = nn.Linear(HIDDEN, 10).cuda()
+model_hybrid.fc1 = nn.Linear(CONV_OUT, HIDDEN).cuda()
+model_hybrid.fc2 = nn.Linear(HIDDEN, NUM_CLASSES).cuda()
 # freeze convolutional layers
 model_hybrid.conv1.weight.requires_grad = False
 model_hybrid.conv2.weight.requires_grad = False
@@ -193,8 +182,8 @@ for epoch in range(1, 10 + 1):
 # change network type
 model_hybrid_alt.net_type = 'hybrid_alt'
 # reinitialize fully connected layers
-model_hybrid_alt.fc1 = nn.Linear(320, HIDDEN).cuda()
-model_hybrid_alt.fc2 = nn.Linear(HIDDEN, 10).cuda()
+model_hybrid_alt.fc1 = nn.Linear(CONV_OUT, HIDDEN).cuda()
+model_hybrid_alt.fc2 = nn.Linear(HIDDEN, NUM_CLASSES).cuda()
 # freeze convolutional layers
 model_hybrid_alt.conv1.weight.requires_grad = False
 model_hybrid_alt.conv2.weight.requires_grad = False
@@ -224,10 +213,7 @@ for i, dataset in enumerate(datasets):
 print('--- Total time: %s seconds ---' % (time.time() - start_time))
 
 torch.save(model_normal, 'models/model_normal.pytorch')
-# torch.save(model_negative, 'models/model_negative.pytorch')
 torch.save(model_negative_relu, 'models/model_negative_relu.pytorch')
 torch.save(model_hybrid, 'models/model_hybrid.pytorch')
 torch.save(model_hybrid_nr, 'models/model_hybrid_nr.pytorch')
 torch.save(model_hybrid_alt, 'models/model_hybrid_alt.pytorch')
-
-print('models saved to "models"')
